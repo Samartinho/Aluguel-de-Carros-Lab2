22cf848717cad0efafcbebfe17315a4873e20419
Bruno Silveira Cequeira Lima <brunobhsclima@gmail.com>
2021-10-03 20:09:35 -0300

    Add files via upload

---
+++ Implementação/powebsys/src/main/java/com/aluguelVeiculos/powebsys/PowebsysApplication.java
@@ -0,0 +1,13 @@
+package com.aluguelVeiculos.powebsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowebsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowebsysApplication.class, args);
+	}
+
+}
